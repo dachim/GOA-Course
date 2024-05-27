f9d5d2186c67bc824044a63ede8539e7c88d2c21
dachim <mefarishvilidachi35@gmail.com>
2024-05-28 00:23:35 +0400

    commit

---
+++ Day 06/Homework/task5.py
@@ -0,0 +1,10 @@
+"""5. მომხმარებელს შემოატანინეთ ორი რიცხვი. თქვენი დავალებაა შექმნათ კალკულატორი,
+ დაბეჭდეთ: რიცხვების ჯამი (num1 + num2), სხვაობა (num1 - num2), ნამრავლი (num1 * num2), განაყოფი (num1 / num2)"""
+
+
+num1 = int(input("enter number: "))
+num2 = int(input("enter number2: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2) 
